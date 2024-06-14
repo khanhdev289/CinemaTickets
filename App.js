9a4496634f735d7d280590d8729a571d2eef1043
Khanh Ngo <106390649+khanhdev289@users.noreply.github.com>
2024-06-14 15:40:45 +0700

    feat/addwelcomescreen (#2)
    Co-authored-by: hoanghoan <133653028+hoan13@users.noreply.github.com>

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomNavication from './src/screens/other/BottomNavication';
 import SignInScreen from './src/screens/start/SignInScreen';
+import SignUpScreen from './src/screens/start/SignUpScreen';
+import ComfirmOTP from './src/screens/start/ComfirmOTP';
+import WelcomeScreen from './src/screens/start/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Home"
           component={BottomNavication}
@@ -18,8 +21,23 @@ export default function App() {
         
         />
         <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
           name="Login"
           component={SignInScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Register"
+          component={SignUpScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Otp"
+          component={ComfirmOTP}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
